22575cd80b345db9d363722ac3895a72cd743e16
cay3428 <86307889+cay3428@users.noreply.github.com>
2022-03-29 15:59:31 +0300

    yoola

--- mfcalsss/datacsessssa/ana/ICategoryDal.cs
+++ mfcalsss/datacsessssa/ana/ICategoryDal.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using varlıksss.cerezz;
 
 namespace DataAccsess.ana
 {
-    interface ICategoryDal
+    interface ICategoryDal:IEntityRespository <Category>
     {
+        //List<Category> GetAll();
+
+        //void Ekle(Category category);
+        //void güncelle(Category category);
+        //void yoket(Category category);
+
+        //List<Category> Getallbycategory(int categoryId);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
