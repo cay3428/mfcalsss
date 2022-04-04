58e2fdda7af5828c5143360d030db772a93e07f4
cay3428 <86307889+cay3428@users.noreply.github.com>
2022-04-04 18:15:49 +0300

    yolla

--- mfcalsss/busiziii/DependencyResolver/Autofac/AutofacBusinessModule.cs
+++ mfcalsss/busiziii/DependencyResolver/Autofac/AutofacBusinessModule.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using Autofac;
+using busiziii.absc;
+using busiziii.concan;
+using DataAccsess.cerezzz.EntityFramework;
+using datacsessssa.ana;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace busiziii.DependencyResolver.Autofac
 {
-    class AutofacBusinessModule
+   public  class AutofacBusinessModule :Module 
+
+       
     {
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal >().SingleInstance();
+
+
+        }
+
     }
 }
